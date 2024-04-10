7738d0ebcc496be13f779c2231d6256c18486c05
codzk <zekemilne@gmail.com>
2024-04-10 19:25:00 +1000

    feat!: generate random pokemon and log it

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 console.log("Terminal app is running!");
 
+const pokemon = require('pokemon');
+
+let randomPokemon = pokemon.random();
+
+console.log(`Today's lucky Pokemon is ${randomPokemon}`);
 console.log("Bye bye, terminal app finished!");
